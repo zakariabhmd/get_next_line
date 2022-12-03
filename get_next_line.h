8d334f45497e960676a466f78c83112624bbe9f6
zakariabhmd <114684860+zakariabhmd@users.noreply.github.com>
2022-12-03 20:23:19 +0100

    Add files via upload

--- get_next_line.h
+++ get_next_line.h
@@ -3,11 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: zbabahmi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: zbabahmi <zbabahmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/12/03 18:55:02 by zbabahmi          #+#    #+#             */
-/*   Updated: 2022/12/03 18:55:03 by zbabahmi         ###   ########.fr       */
+/*   Created: 2022/12/03 20:15:57 by zbabahmi          #+#    #+#             */
+/*   Updated: 2022/12/03 20:15:58 by zbabahmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
 
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h>
