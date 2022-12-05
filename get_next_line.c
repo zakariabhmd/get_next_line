976df8939835a3d32b52848572041977088ddcfb
zakariabhmd <114684860+zakariabhmd@users.noreply.github.com>
2022-12-05 19:48:21 +0100

    Add files via upload

--- get_next_line.c
+++ get_next_line.c
@@ -6,9 +6,25 @@
 /*   By: zbabahmi <zbabahmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 20:15:47 by zbabahmi          #+#    #+#             */
-/*   Updated: 2022/12/03 20:15:48 by zbabahmi         ###   ########.fr       */
+/*   Updated: 2022/12/05 19:40:45 by zbabahmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+int ft_newline(char *str)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (-1);
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			return (i);
+		i++;
+	}
+	return (-1);
+}
 
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,24 @@
 /*   By: zbabahmi <zbabahmi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/03 20:15:57 by zbabahmi          #+#    #+#             */
-/*   Updated: 2022/12/03 20:15:58 by zbabahmi         ###   ########.fr       */
+/*   Updated: 2022/12/05 19:46:53 by zbabahmi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdio.h>
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE
+#endif
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
+
+size_t	ft_strlen(const char *str);
+char	*ft_join(char const *s1, char const *s2);
+char	*ft_strdub(const char *s1);
+char	*ft_substr(char const *S, unsigned int start, size_t len);
+
+#endif
